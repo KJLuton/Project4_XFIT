63d0fdc811ba5e520da99e2a23f7e075ca7ad57d
Kirstyn <kirstyn.jenna@hotmail.co.uk>
2022-05-13 10:16:32 +0000

    Added static_root

--- xfit_box/settings.py
+++ xfit_box/settings.py
@@ -172,6 +172,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
